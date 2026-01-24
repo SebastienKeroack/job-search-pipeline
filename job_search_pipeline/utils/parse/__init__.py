5892e75f0a09fb447f0ba0a4d17c441474418e8a
Sébastien Kéroack <dev@sebastienkeroack.com>
2026-01-24 16:55:39 -0500

    feat(parse): implement job level transformation and add tests

--- job_search_pipeline/utils/parse/__init__.py
+++ job_search_pipeline/utils/parse/__init__.py
@@ -2,6 +2,7 @@
 #                       Copyright 2026, Sébastien Kéroack
 # ==============================================================================
 
+from . import job_level
 from . import salary
 
-__all__ = ["salary"]
+__all__ = ["job_level", "salary"]
